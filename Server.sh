cfdb8edcf253177fd9fe97fcc7ed01003c069c41
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-07 05:27:22 +0200

    Update Server.sh

--- Server.sh
+++ Server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+
+mem=$(grep MemTotal /proc/meminfo | sed -e 's/MemTotal:[ ]*//' | sed -e 's/ kB//') # some new stuff 
+mem=$(($mem/1024/1024))
+mem=$(($mem/3))
+
+$JAVA -Xmx${mem}G -Xms${mem}G -jar server.jar nogui
+
 powerprofilesctl set performance
 
 # Start playit in a new Konsole window and detach immediately
