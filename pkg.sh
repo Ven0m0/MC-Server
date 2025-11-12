3caf131951be83dac0c57cb58903fadd24a361c9
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-13 00:59:36 +0100

    Delete pkg.sh

--- pkg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install from AUR using paru
-paru --noconfirm --skipreview -Sq ferium jdk25-graalvm-bin
-
-# Install remaining packages from official repos in a single operation
-sudo pacman --noconfirm -Sq gamemode preload prelockd nohang memavaild adaptivemm uresourced
