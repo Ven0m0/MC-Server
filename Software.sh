192d6d62ea4f3f7961dda46ffdbc3c292273f4fb
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-13 01:00:06 +0100

    Delete Software.sh

--- Software.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script now calls infrarust.sh which handles the complete infrarust installation
-# https://crates.io/crates/infrarust
-
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-"$SCRIPT_DIR/infrarust.sh"
