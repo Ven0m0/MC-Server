cdf3d358731158d6cdeb2d6dde150385e02cdd72
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-13 07:40:29 +0100

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# Install from AUR using paru
-paru --noconfirm --skipreview -Sq ferium jdk25-graalvm-bin
-
-# Install remaining packages from official repos in a single operation
-sudo pacman --noconfirm -Sq gamemode preload prelockd nohang memavaild adaptivemm uresourced
+# Install dependencies
+paru --noconfirm --skipreview -Sq ferium jdk25-graalvm-bin gamemode preload prelockd nohang memavaild adaptivemm uresourced
 
 # For the server
 java -Xms10G -Xmx10G -XX:ArchiveClassesAtExit=minecraft_server.jsa -jar server.jar
