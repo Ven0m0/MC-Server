09845376ba6c569eee3bdf5c0e19f9a1cbedc0a2
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 03:03:16 +0100

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -7,4 +7,4 @@ paru --noconfirm --skipreview -Sq ferium jdk25-graalvm-bin gamemode preload prel
 java -Xms10G -Xmx10G -XX:ArchiveClassesAtExit=minecraft_server.jsa -jar server.jar
 
 # For the client
-java -Xms4G -Xmx4G -XX:ArchiveClassesAtExit=minecraft_client.jsa -jar client.jar
+java -Xms10G -Xmx10G -XX:ArchiveClassesAtExit=minecraft_client.jsa -jar client.jar
