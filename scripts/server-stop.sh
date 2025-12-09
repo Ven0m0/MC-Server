11a7569bb92983cc29abf3f8574c8b3b3209e6f9
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-09 22:52:50 +0100

    Create server-stop.sh

---
+++ scripts/server-stop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# shellcheck enable=all shell=bash source-path=SCRIPTDIR external-sources=true
+set -euo pipefail; shopt -s nullglob globstar
+IFS=$'\n\t' LC_ALL=C
+has(){ command -v -- "$1" &>/dev/null; }
+
+# TODO stop server
